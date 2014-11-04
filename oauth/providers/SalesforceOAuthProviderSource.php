b15dd62e916a087fd3de5b4193dfa45b94318b1d
Benjamin David <ben@dukt.net>
2014-11-04 21:32:12 +0100

    Added SalesforceOAuthProviderSource

---
+++ oauth/providers/SalesforceOAuthProviderSource.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Craft OAuth by Dukt
+ *
+ * @package   Craft OAuth
+ * @author    Benjamin David
+ * @copyright Copyright (c) 2014, Dukt
+ * @license   https://dukt.net/craft/oauth/docs/license
+ * @link      https://dukt.net/craft/oauth/
+ */
+
+namespace OAuthProviderSources;
+
+class SalesforceOAuthProviderSource extends BaseOAuthProviderSource {
+
+    public $consoleUrl = 'https://developer.vimeo.com/apps';
+
+    public function getName()
+    {
+        return 'Salesforce';
+    }
+
+    public function getAccount()
+    {
+        try {
+
+            $response = $this->service->request('/me');
+            $response = json_decode($response, true);
+
+            $account = array();
+
+            $account['uid'] = substr($response['uri'], strrpos($response['uri'], "/") + 1);
+            $account['name'] = $response['name'];
+
+            return $account;
+        }
+        catch(\Exception $e)
+        {
+            // todo
+            throw new \Exception($e, 1);
+
+        }
+    }
+}
